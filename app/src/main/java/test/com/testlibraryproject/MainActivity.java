9a87de1350a0baa51fcdc79e1ccde5fc70cded9f
vineeth <vineethkumar0791@gmail.com>
2016-10-26 17:41:30 +0530

    testing

--- app/src/main/java/test/com/testlibraryproject/MainActivity.java
+++ app/src/main/java/test/com/testlibraryproject/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Context context=MainActivity.this;
 
         TestLibrary testLibrary=new TestLibrary(MainActivity.this);
-        testLibrary.displaySurvey("First sample after uploading to git","THis is working");
+        testLibrary.displaySurvey("First testig sample after uploading to git","THis is working");
 
     }
 }
